7a20bfbf81622f23693e0f91e68760e403d9054a
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-01-02 14:30:54 -0300

    initial commit

---
+++ Chat Language iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat Language iOS
+//
+//  Created by Vinicius Mangueira on 02/01/2019.
+//  Copyright © 2019 Vinicius Mangueira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
